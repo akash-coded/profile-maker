858ff1a3a8e97ea0e3e0633411d304c5f4c68749
Akash Das <mfs.akash@gmail.com>
2020-01-24 19:24:58 +0530

    Page Footer

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+    <footer class="copyright py-2 text-center text-white fixed-bottom">
+        <div class="container">
+            <small>Copyright &copy; Profile Maker - Designed by Akash Das</small>
+        </div>
+    </footer>
